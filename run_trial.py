35991d9d79451d2b4ab80b778fe8a68e9e3fbd3a
sylvestermac <s.marelli@yahoo.com>
2017-08-06 13:00:52 +0200

    Finished Scrapit
    and starting writeit

--- run_trial.py
+++
@@ -1,3 +0,0 @@
-import scrapit as sc
-
-sc.scrapWgb('http://www.worldgovernmentbonds.com/country/germany/')
--- scrapit.py
+++ scrapit.py
@@ -1,11 +1,35 @@
 import time
 import requests
 from bs4 import BeautifulSoup as bs
+##### start/divert all prints to a log file
+import sys
+old_stdout = sys.stdout
 
-dataFormatGlobal = {"Ticker": "","FullName": "","Date": "",
-                    "Price": "","Open": "",
-                    "DayH": "","DayL": "","52H": "","52L": "","Volume": "","OpenInterest": "",
-                    "datacheck": "TNPODD55VI"}
+log_file = open("message.log","w")
+
+sys.stdout = log_file
+##### start\divert all prints to a log file
+
+
+#data shared format between Marketwatch and Bloomberg
+dataFormatGlobal = {
+    "Ticker": "",
+    "FullName": "",
+    "Date": "",
+    "Price": "",
+    "Open": "",
+    "DayH": "",
+    "DayL": "",
+    "52H": "",
+    "52L": "",
+    "Volume": "",
+    "OpenInterest": ""}
+
+dataFormatGlobalCDS = {
+    'Name': '',
+    'Value': '',
+    'Unit': '',
+    'bptChange': ''}
 
 def durataTranslator(text):
     text = text.replace(' ','').replace('\n','')
@@ -53,11 +77,112 @@ def mkTranslator(text):
     return int(num)
 
 ##
-#/ scraping function
+#/ scraping functions
 ##
 
-def scrapWgb(address):
+def scrapSovereignCDS(address):
+    r = requests.get(address)
+    c = r.content
+    sup = bs(c,"html.parser")
 
+    table = sup.find("table",{"id": lambda x: x and 'TitoliStato' in x})
+    table = table.find('tbody')
+    rows = table.find_all("tr")
+
+    sovereignCDS = []
+
+    for row in rows:
+        dictRow = dataFormatGlobalCDS
+
+        name = row.find("td",{"class": lambda x: x and 'Nome' in x})
+        name = name.text.replace(' ','').replace('\n','').replace('\t','')
+
+        value = row.find("td",{"class": lambda x: x and 'Rendimento' in x})
+        value = value.text.replace(' ','').replace('\n','').replace('\t','')
+
+        dictRow['Name'] = name
+        dictRow['Value'] = value
+        dictRow['Unit'] = 'Spread'
+        dictRow['bptChange'] = ''
+
+        sovereignCDS.append(dictRow)
+
+    return sovereignCDS  #ritorna una lista di dizionari 'dataFormatGlobalCDS'
+
+def scrapWsj(address):
+    #CDS indexes and big movers for bonds and corporate
+    r = requests.get(address)
+    c = r.content
+    sup = bs(c,"html.parser")
+
+    iframes = sup.find_all("iframe")
+
+    # finds the address where the real table is kept, the real content of iframe
+    # and checks if is the only address where markit appears, if not things changed in the page
+    src4iframe = 'none'
+    for iframe in iframes:
+        try:
+            src = iframe.attrs['src']
+            if 'markit' in src:              #crucial if website changes
+                if src4iframe == 'none':
+                    src4iframe = src
+        except:
+            pass
+    #check finished we now have an address (srcJust) or 'none'
+
+    html = requests.get(src4iframe).text
+    soup = bs(html,'html.parser')
+
+    #take values for all of the tables  ### crucial if website change
+    try:
+        names = soup.findAll("td",{"class": lambda x: x and 'col1' in x})
+        values = soup.findAll("td",{"class": lambda x: x and 'col2' in x})
+        unit = soup.findAll("td",{"class": lambda x: x and 'col3' in x})
+        bptChange = soup.findAll("td",{"class": lambda x: x and 'col5' in x})
+    except:
+        print("table CDS column gathering did not work")
+
+    dictCDS = []
+    dictCDSbondsBM = []   #BM as in Big Movers
+    dictCDSstockBM = []
+
+    for i in range(0,12):
+        if 'n.a.' not in values[i].text:
+            dictRow = dataFormatGlobalCDS
+
+            dictRow['Name'] = names[i].text
+            dictRow['Value'] = values[i].text
+            dictRow['Unit'] = unit[i].text
+            dictRow['bptChange'] = ''
+
+            dictCDS.append(dictRow)
+
+    #tigheners/wideners bonds (last 20 rows we take first ten)
+    for i in range(-20,-10):
+        dictRow = dataFormatGlobalCDS
+
+        dictRow['Name'] = names[i].text
+        dictRow['Value'] = values[i].text
+        dictRow['Unit'] = 'Spread'
+        dictRow['bptChange'] = bptChange[i].text
+
+        dictCDSbondsBM.append(dictRow)
+
+    #tigheners/wideners bonds (last 20 rows we take first ten)
+    for i in range(-10,0):
+        dictRow = dataFormatGlobalCDS
+
+        dictRow['Name'] = names[i].text
+        dictRow['Value'] = values[i].text
+        dictRow['Unit'] = 'Spread'
+        dictRow['bptChange'] = bptChange[i].text
+
+        dictCDSstockBM.append(dictRow)
+
+    return dictCDS, dictCDSbondsBM, dictCDSstockBM  #ritorna tre liste di dizionari 'dataFormatGlobalCDS'
+
+def scrapWgb(address):
+    #for yeld curves
     r = requests.get(address)
     c = r.content
     sup = bs(c,"html.parser")
@@ -88,7 +213,9 @@ def scrapWgb(address):
 
     return maturity, yields
 
-def scrapCmegroup(address, maxRows = 30, switchOI4eachMonth = True):
+def scrapCmegroup(address):
+    maxRows = 30,
+    switchOI4eachMonth = True
 
     #creating/formatting data from scrapdata
     r = requests.get(address)
@@ -108,40 +235,48 @@ def scrapCmegroup(address, maxRows = 30, switchOI4eachMonth = True):
         selectedCELLS = row.find_all("td",{"id": lambda x: x and 'quotesFuturesProductTable1' in x})
 
         data = {"Ticker": "",
-                "last": "","priorSettle": "","open": "","high": "","low": "","volume": "",
-                "datacheck": "OK"}
+                "last": "",
+                "priorSettle": "",
+                "open": "",
+                "high": "",
+                "low": "",
+                "volume": "",
+                "openInterest": "",
+                "blockTrades": ""}
 
-        totalValues = {'totalOpenInterest': "",'totalVolume': "",'totalBlockTrades': ""}
+        totalValues = {'totalOpenInterest': "",
+                       'totalVolume': "",
+                       'totalBlockTrades': ""}
 
         try:
+            #the id of the tag in the first TD has the ticker of the instrument i.e. quotesFuturesProductTable1_6EQ7_change
             data["Ticker"] = selectedCELLS[0].get('id').split("_")[1]
+            if not data["Ticker"][-1].isdigit():  #small check to see if we actualy got a ticker code
+                print("'{}' No 'Ticker' CME".format(address))
         except:
-            data["datacheck"] = "-NO_ticker-"
             print("'{}' No 'Ticker' CME".format(address))
 
         for j, cell in enumerate(selectedCELLS):
 
             try:
-                label = cell.get('id').split("_")[2]
+                label = cell.get('id').split("_")[2] ###taking all teb labels: quotesFuturesProductTable1_6EQ7_open
             except:
-                data["datacheck"] += "-NO_label-"
-                print("'{}' No 'label' CME".format(address))
+                print("'{}' No 'label' CME '_' split did not work".format(address))
 
             for key in data:
-                if key == label:
+                if key == label: ## if one of the label from the dictonary matches any of the labe just gathrrd, then add value to that key
                     try:
-                        stringValue = cell.text.replace(",","")
-                        if stringValue != "-":
+                        stringValue = cell.text.replace("-","").replace(",","")
+                        if stringValue != "":
                             float(stringValue)
                     except:
-                        data["datacheck"] += "-NO_float-"
                         print("'{}' No 'float' CME".format(address))
 
                     data[key] = stringValue
 
         table.append(data)
 
-    #// from now Volume page to get Total and OpenInterest
+    #// from here down Volume page to get Total and OpenInterest
 
     r = requests.get(address.replace('.html','_quotes_volume_voi.html'))    ### crucial if website change
     c = r.content
@@ -150,51 +285,47 @@ def scrapCmegroup(address, maxRows = 30, switchOI4eachMonth = True):
     tableSup = sup.find("tbody")
     rows = tableSup.find_all("tr")
 
-    totalOpenInterest = '--'
-    totalVolume = '--'
-    totalBlockTrades = '--'
+    totalOpenInterest = ''
+    totalVolume = ''
+    totalBlockTrades = ''
 
     colNumOpenInterest = 10     ### crucial if website change
     colNumVolume = 3     ### crucial if website change
     colNumBlockTrades = 4     ### crucial if website change
 
-    if rows[-1].find('th').text == 'Totals':
+    if rows[-1].find('th').text == 'Totals':   ##last row of the table
         cells = rows[-1].find_all('td')
         totalOpenInterest = cells[colNumOpenInterest].text.replace(',','')
         totalVolume = cells[colNumVolume].text.replace(',','')
         totalBlockTrades = cells[colNumBlockTrades].text.replace(',','')
-
-
-
+    else:
+        print("'{}' No 'Totals' CME tag was found".format(address))
     #// from now if switch is on we scrap OpenInterest and maybe OIchange for each month
-    ioTable = []
 
+    ioTable = []
     if switchOI4eachMonth:
         mapMonths = {'JAN': 'F', 'FEB': 'G', 'MAR': 'H', 'APR': 'J',
                      'MAY': 'K', 'JUN': 'M', 'JUL': 'N', 'AUG': 'Q',
                      'SEP': 'U', 'OCT': 'V', 'NOV': 'X', 'DEC': 'Z',}
 
         for row in rows:
+            tag = row.find('th').text                          #there is only one th per row and is the ticker text: SEP 17
+            for key in mapMonths:                              #
+                if tag.split(' ')[0] == key:                   #SEP
+                    yearLastDigit = tag.split(' ')[-1][-1]     #7
+                    tag = mapMonths[key] + yearLastDigit       #U+7
 
-            tag = row.find('th').text
-            for key in mapMonths:
-                if tag.split(' ')[0] == key:
-                    yearLastDigit = tag.split(' ')[1][1]
-                    tag = mapMonths[key] + yearLastDigit
-                    for dic in table:
-                        thisTableMonth = dic['Ticker'][-2] + dic['Ticker'][-1]
+                    for dic in table:                          #in this for cycle we look at every dic in tabel and when ticker matches our tag, we add open interest and block trades
+                        thisTableMonth = dic['Ticker'][-2] + dic['Ticker'][-1]     #GEU7  ---> U+7
                         if thisTableMonth == tag:
                             cells = row.find_all('td')
-                            oiData = {'Ticker': dic['Ticker'],
-                                      'openInterest': cells[colNumOpenInterest].text.replace(',',''),
-                                      'blockTrades': cells[colNumBlockTrades].text.replace(',','')}
-                            ioTable.append(oiData)
-        #// we now add Open Interest and Block Trades to table{}
+                            dic['openInterest'] = cells[colNumOpenInterest].text.replace(',','')
+                            dic['blockTrades'] = cells[colNumBlockTrades].text.replace(',','')
 
-    return totalValues, table, ioTable
+    return totalValues, table  #ritorna un piccolo dizionario ed una lista di dizionari (un dizionari for every expiry)
 
 def scrapMarketwatch(address):
-    #creating/formatting data from scrapdata
+    #creating formatting data from scrapdata
     r = requests.get(address)
     c = r.content
     sup = bs(c,"html.parser")
@@ -205,11 +336,7 @@ def scrapMarketwatch(address):
     data = dataFormatGlobal
 
     if not len(lab) == len(val):
-        print('labels and values mismatch')
-        string = ""
-        for char in data["datacheck"]:
-            string += "-"
-        data["datacheck"] = string
+        print("'{}' labels and values mismatch".format(address))
         return data
 
     scrapData = {}
@@ -223,52 +350,44 @@ def scrapMarketwatch(address):
     try:
         data["Ticker"] = sup.find("span",{"class": "company__ticker"}).text.replace(' ','')
     except:
-        data["datacheck"] = data["datacheck"].replace("T","-")
         print("'{}' No 'Ticker'".format(address))
 
     try:
         data["FullName"] = sup.find("span",{"class":"company__market"}).text.replace(' ','')
     except:
-        data["datacheck"] = data["datacheck"].replace("N","-")
         print("'{}' No 'FullName'".format(address))
 
     try:
         data["Price"] = sup.find("span",{"class":"value"}).text.replace(",","").replace(" ","")
         int_try = float(data["Price"]) #check that an actual number was found for Price
     except:
-        data["datacheck"] = data["datacheck"].replace("P","-")
         print("'{}' No 'Price'".format(address))
 
     try:
         data["Open"] = scrapData["Open"].replace(" ","")
     except:
-        data["datacheck"] = data["datacheck"].replace("O","-")
         print("'{}' No 'Open'".format(address))
 
     try:
         data["DayH"] = scrapData["DayRange"].replace(" - ",";;").replace(" ","").split(";;")[1]
         data["DayL"] = scrapData["DayRange"].replace(" - ",";;").replace(" ","").split(";;")[0]
     except:
-        data["datacheck"] = data["datacheck"].replace("D","-")
         print("'{}' No 'DayRange'".format(address))
 
     try:
         data["52H"] = scrapData["52WeekRange"].replace(" - ",";;").replace(" ","").split(";;")[1]
         data["52L"] = scrapData["52WeekRange"].replace(" - ",";;").replace(" ","").split(";;")[0]
     except:
-        data["datacheck"] = data["datacheck"].replace("5","-")
         print("'{}' No '52WeekRange'".format(address))
 
     try:
         data["Volume"] = str(mkTranslator(sup.find("span",{"class":"volume last-value"}).text))
     except:
-        data["datacheck"] = data["datacheck"].replace("V","-")
         print("'{}' No 'Volume'".format(address))
 
     try:
         data["OpenInterest"] = scrapData["OpenInterest"].replace(" ","")
     except:
-        data["datacheck"] = data["datacheck"].replace("I","-")
         print("'{}' No 'OpenInterest'".format(address))
 
     return data;
@@ -285,64 +404,52 @@ def scrapBloomberg(address):
     data = dataFormatGlobal
 
     if not len(lab) == len(val):
-        print('labels and values mismatch')
-        string = ""
-        for char in data["datacheck"]:
-            string += "-"
-        data["datacheck"] = string
+        print("'{}' labels and values mismatch".format(address))
         return data
 
     scrapData = {}
-
+    #we create a dictionary of scraped data with labels as key and val as values
     for i, key in enumerate(lab):
         scrapData[key.text.replace(" ","")] = val[i].text.replace(",","").replace("%","").replace("$","").replace("£","").replace("€","")
-    print(scrapData)
 
     data["Date"] = time.strftime("%Y/%m/%d %H:%M %a")
 
     try:
         data["Ticker"] = sup.find("div",{"class": "ticker"}).text.replace(' ','')
     except:
-        data["datacheck"] = data["datacheck"].replace("T","-")
-        print("No 'Ticker' for '{}'".format(address))
+        print('{}' "No 'Ticker'".format(address))
 
     try:
         data["FullName"] = sup.find("h1",{"class":"name"}).text.replace(' ','')
     except:
-        data["datacheck"] = data["datacheck"].replace("N","-")
-        print("No 'FullName' for '{}'".format(address))
+        print("'{}' No 'FullName'".format(address))
 
     try:
         data["Price"] = sup.find("div",{"class":"price"}).text.replace(",","").replace(" ","")
     except:
-        data["datacheck"] = data["datacheck"].replace("P","-")
-        print("No 'Price' for '{}'".format(address))
+        print("'{}' No 'Price'".format(address))
 
     try:
         data["Open"] = scrapData["Open"].replace(" ","")
     except:
-        data["datacheck"] = data["datacheck"].replace("O","-")
-        print("No 'Open' for '{}'".format(address))
+        print("'{}' No 'Open'".format(address))
 
     try:
         data["DayH"] = scrapData["DayRange"].replace(" - ",";;").replace(" ","").split(";;")[1]
         data["DayL"] = scrapData["DayRange"].replace(" - ",";;").replace(" ","").split(";;")[0]
     except:
-        data["datacheck"] = data["datacheck"].replace("D","-")
-        print("No 'DayRange' for '{}'".format(address))
+        print("'{}' No 'DayRange'".format(address))
 
     try:
         data["52H"] = scrapData["52WkRange"].replace(" - ",";;").replace(" ","").split(";;")[1]
         data["52L"] = scrapData["52WkRange"].replace(" - ",";;").replace(" ","").split(";;")[0]
     except:
-        data["datacheck"] = data["datacheck"].replace("5","-")
-        print("No '52WkRange' for '{}'".format(address))
+        print("'{}' No '52WkRange'".format(address))
 
     try:
         data["Volume"] = scrapData["Volume"].replace(" ","")
     except:
-        data["datacheck"] = data["datacheck"].replace("V","-")
-        print("No 'Volume' for '{}'".format(address))
+        print("'{}' No 'Volume'".format(address))
 
     ''' in Bloomberg non ce mai Oi
     try:
@@ -355,7 +462,14 @@ def scrapBloomberg(address):
     return data;
 
 ##
-#\ scraping function
+#\ scraping functions
 ##
 
-print(scrapWgb('http://www.worldgovernmentbonds.com/country/germany/'))
+##### end/divert all prints to a log file
+sys.stdout = old_stdout
+
+log_file.close()
+##### end\divert all prints to a log file
+t = scrapCmegroup('http://www.cmegroup.com/trading/metals/base/copper.html')[1]
+for ti in t:
+    print(ti)
