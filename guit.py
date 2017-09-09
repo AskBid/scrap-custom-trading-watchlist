71bf4e9faaaa00990914dc0f34a62b3f0b14a447
sylvestermac <s.marelli@yahoo.com>
2017-09-09 18:45:20 +0100

    datait-begins
    datait-begins first step to a complete interface

--- guit.py
+++ guit.py
@@ -3,6 +3,7 @@ from os import listdir
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import QCoreApplication, QRect, Qt
+import datait
 
 class Column(QTextEdit):
 
@@ -11,57 +12,75 @@ class Column(QTextEdit):
 
         self.setText(self.write_label())
 
+        self.adjustSize()
+        self.setStyleSheet("""
+        QTextEdit {
+            border: 0;
+            background-color: #838ea0;
+            margin: 0px; padding-left:0;
+            padding-top:0;
+            padding-bottom:0;
+            padding-right:0;
+        }
+        """)
+
 
     def write_label(self):
-        string = ''
+
         name_list = self.read_col_list(0)
-        for name in name_list:
-            
+        text = ''
 
-        '''
-        /*--bgcolor--*/
-        <!--name-->
-        <!--prc_C-->
-        <!--prc_O-->
-        <!--prc-->
-        <!--val03-->
-        <!--avg03-->
-        <!--val02-->
-        <!--avg02-->
-        <!--val01-->
-        <!--avg01-->
-        <!--val00-->
-        <!--avg00-->
-        <!--val13-->
-        <!--avg13-->
-        <!--val12-->
-        <!--avg12-->
-        <!--val11-->
-        <!--avg11-->
-        <!--val10-->
-        <!--avg10-->
-        '''
 
-        # with open('gui/elementH.html') as f:
-    	#        html = f.read()
-        # with open('gui/style.css') as f:
-    	#        css = f.read()
+        thisFileData = datait.Calc_Vals(name_list[0])
+
+        with open('gui/label_H.html') as f:
+    	       html = f.read()
+        html = html.replace('/*--bgcolor--*/', 'rgba(119, 212, 214, 0.7)')
+        html = html.replace('<!--name-->', str(thisFileData.file_name))
+        html = html.replace('<!--prc-->', str(thisFileData.price))
+
+        html = html + html
+        html = html.replace('rgba(119, 212, 214, 0.7)','rgba(240, 212, 2, 0.7)')
+
+        return html
+
+        # for name in name_list:
+        #     thisFileData = datait.Calc_Vals(name)
+        #     with open('gui/label_H.html') as f:
+        # 	       html = f.read()
+        #     with open('gui/label_style.css') as f:
+        # 	       css = f.read()
+        #     htmlcss = css + html
+        #     htmlcss.replace('<!--name-->', str(thisFileData.file_name))
+        #     htmlcss.replace('<!--prc-->', str(thisFileData.getLastPrc))
         #
-        # string = css + html
-        #
-        # self.setText(string + string)
-        #
-        # self.adjustSize()
-        # self.setStyleSheet("""
-        # QTextEdit {
-        #     border: 0;
-        #     background-color: #838ea0;
-        #     margin: 0px; padding-left:0;
-        #     padding-top:0;
-        #     padding-bottom:0;
-        #     padding-right:0;
-        # }
-        # """)
+        #     text = text + htmlcss
+
+        return text
+
+
+
+        # /*--bgcolor--*/
+        # <!--name-->
+        # <!--prc_C-->
+        # <!--prc_O-->
+        # <!--prc-->
+        # <!--val03-->
+        # <!--avg03-->
+        # <!--val02-->
+        # <!--avg02-->
+        # <!--val01-->
+        # <!--avg01-->
+        # <!--val00-->
+        # <!--avg00-->
+        # <!--val13-->
+        # <!--avg13-->
+        # <!--val12-->
+        # <!--avg12-->
+        # <!--val11-->
+        # <!--avg11-->
+        # <!--val10-->
+        # <!--avg10-->
 
 
     def read_col_list(self, col):
