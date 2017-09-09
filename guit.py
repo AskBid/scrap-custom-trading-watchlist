b2ebd8fb9ec11a21aae23e2404db7f68ce1a964f
sylvestermac <s.marelli@yahoo.com>
2017-09-09 17:10:22 +0100

    tidyup
    ordered files as we can seriously start with gui coding

--- guit.py
+++ guit.py
@@ -1,32 +1,108 @@
-from tkinter import *
+import sys
+from os import listdir
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QCoreApplication, QRect, Qt
+
+class Column(QTextEdit):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setText(self.write_label())
 
 
-class ValueAverage:
+    def write_label(self):
+        string = ''
+        name_list = self.read_col_list(0)
+        for name in name_list:
+            
 
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack
+        '''
+        /*--bgcolor--*/
+        <!--name-->
+        <!--prc_C-->
+        <!--prc_O-->
+        <!--prc-->
+        <!--val03-->
+        <!--avg03-->
+        <!--val02-->
+        <!--avg02-->
+        <!--val01-->
+        <!--avg01-->
+        <!--val00-->
+        <!--avg00-->
+        <!--val13-->
+        <!--avg13-->
+        <!--val12-->
+        <!--avg12-->
+        <!--val11-->
+        <!--avg11-->
+        <!--val10-->
+        <!--avg10-->
+        '''
 
-        self.valueLab = Label(master, text = 'value', bg = 'green')
-        self.averageLab = Label(master, text = 'average', bg = 'yellow')
-        self.valueLab.grid(row = 0, sticky=NE)
-        self.averageLab.grid(row = 1, sticky=NE)
-        Grid.columnconfigure(frame, 1, weight=1)
-        Grid.columnconfigure(frame, 0, weight=1)
+        # with open('gui/elementH.html') as f:
+    	#        html = f.read()
+        # with open('gui/style.css') as f:
+    	#        css = f.read()
+        #
+        # string = css + html
+        #
+        # self.setText(string + string)
+        #
+        # self.adjustSize()
+        # self.setStyleSheet("""
+        # QTextEdit {
+        #     border: 0;
+        #     background-color: #838ea0;
+        #     margin: 0px; padding-left:0;
+        #     padding-top:0;
+        #     padding-bottom:0;
+        #     padding-right:0;
+        # }
+        # """)
 
-root = Tk()
+
+    def read_col_list(self, col):
+        col_list = []
+
+        with open('gui/guilist.csv') as f:
+            csv = f.readlines()
+
+        for line in csv:
+            line = line.replace('\n','').split(',')
+            file_name = line[col]
+            col_list.append(file_name)
+
+        return col_list
 
 
-name = Label(root, text='SPX.i')
-price = Label(root, text='2451.54')
-frame_1 = Frame(root)
-frame_2 = Frame(root)
-range52 = Canvas(root, height = 3, bg='red')
-a = ValueAverage(frame_1)
-b = ValueAverage(frame_2)
 
-frame_1.pack()
+class MainWindow(QScrollArea):
+    def __init__(self):
+        super().__init__()
+
+        layout = QHBoxLayout(self)
+        layout.setAlignment(Qt.AlignTop)
+        layout.addWidget(Column())
+        # layout.addWidget(Column())
+        # layout.addWidget(Column())
+        # layout.addWidget(Column())
+        # layout.addWidget(Column())
 
 
+        layout.setContentsMargins(0,0,0,0)
+        layout.setSpacing(0)
+        self.setWindowTitle('SnP watchlist')
 
-root.mainloop()
+
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    #print(QDesktopWidget().availableGeometry())
+    ex = MainWindow()
+    sys.exit(app.exec_())
