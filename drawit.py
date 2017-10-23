a6cb68373521da4cca9142b473ffd3a47dceb502
AskBid <sergio.mrll@gmail.com>
2017-10-23 18:53:49 -0400

    to laptop

--- drawit.py
+++ drawit.py
@@ -60,64 +60,81 @@ def drawCandle( lenght = 226,
                 avgRange = 10.17,
                 dayRange = 19,
 
-                yClose = 2560, #12935.5  52rH
-                yOpen = 2556,
-                yLow = 2556.25,
-                yHigh = 2562.25,
+                yClose = 2560, #taken from [-1]
+                yOpen = 2556.50, #taken from [-2]
+                yLow = 2556.25, #taken from [-2]
+                yHigh = 2562.25, #taken from [-2]
 
-                dayOpen = 2560, #day open
-                dayClose = 2560.75,
-                dayLow = 2542.5,
-                dayHigh = 2561.5,
+                dayOpen = 2560, #taken from [-1]
+                price = 2560.75, #taken from [-1]
+                dayLow = 2542.5, #taken from [-1]
+                dayHigh = 2561.5, #taken from [-1]
                 path = "gui/candle.png"):
+
+    def background():
+        cr.set_source_rgba(0.4, 0.4, 0.4, 0.5)
+        cr.set_line_width(0)
+        cr.rectangle(0, 0, thickness, lenght)
+        cr.set_line_join(cairo.LINE_JOIN_MITER)
+        cr.fill()
+        cr.stroke()
 
     centerY = lenght / 2
     centerX = thickness / 2
     ratio = dayRange / avgRange
-    pts2pxs_rt = 0
-
-    if ratio < 3:
-        ## find max points that fit inside canvas
-        maxPts = avgRange * 3
-        pts2pxs_rt = lenght / maxPts
-
-    if ratio >= 3:
-        maxPts = dayRange
-        pts2pxs_rt = lenght / maxPts
+    pt2px_rt = 0
+    line_w = 2
+    arm_len = 3
+    deltaOL = dayOpen - dayLow
+    deltaOH = price - dayLow
 
     ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, thickness, lenght)
     cr = cairo.Context(ims)
     cr.move_to(centerX,centerY)
     cr.scale(1,-1)
     cr.translate(0, -lenght)
+    cr.set_line_join(cairo.LINE_CAP_SQUARE)
+    background()
 
-    cr.set_source_rgba(0.4, 0.4, 0.4, 0.5)
+    if ratio < 3:
+        ## find max points that fit inside canvas
+        maxPts = avgRange * 3
+        pt2px_rt = lenght / maxPts
+
+    if ratio >= 3:
+        maxPts = dayRange
+        pt2px_rt = lenght / maxPts
+
+    def pt2px(pts):
+        return pts * pt2px_rt
+
+    cr.set_source_rgb(0, 0, 0)
     cr.set_line_width(0)
-    cr.rectangle(0, 0, thickness, lenght)
-    cr.set_line_join(cairo.LINE_JOIN_MITER)
+    bar_len = pt2px(dayRange)
+    half_bar = bar_len / 2
+    bar_low = centerY - half_bar
+    xBar = centerX + line_w
+    cr.set_line_width(line_w)
+
+    #today bar trunk
+    cr.rectangle(0, 5, 2, 9)
+    cr.rectangle(int(xBar), int(bar_low), int(line_w), int(bar_len))
+    # cr.move_to(xBar, bar_low)
+    # cr.line_to(xBar, bar_low + bar_len)
+
+    #today arm open
+    # cr.rectangle(int(xBar), int(bar_low + pt2px(deltaOL)), int(xBar - arm_len), int(bar_low + pt2px(deltaOL)))
+    # cr.move_to(xBar, bar_low + pt2px(deltaOL))
+    # cr.line_to(xBar - arm_len, bar_low + pt2px(deltaOL))
+
+    #today arm current/close
+    cr.move_to(xBar, bar_low + pt2px(deltaOH))
+    cr.line_to(xBar + arm_len, bar_low + pt2px(deltaOH))
+
     cr.fill()
     cr.stroke()
-
-    def pts2px(max_L):
-        pass
-
-    def lineByCenter(bar_len):
-        half_bar = bar_len / 2
-        start = centerY - half_bar
-
-        cr.set_source_rgb(0, 0, 0)
-        cr.set_line_width(2)
-
-        cr.move_to(centerX, start)
-        cr.line_to(centerX, start + bar_len)
-        cr.stroke()
-
-    drawLineCenter(16)
-
-    ratio = dayRange / avgRange
-
-
     ims.write_to_png(path)
+
 
 if __name__ == "__main__":
     drawCandle()
